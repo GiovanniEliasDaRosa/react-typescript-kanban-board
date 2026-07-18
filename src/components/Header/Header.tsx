b697ad4cc6097b280d54bdc31a9e70e4bd406ef4
Giovanni <101050472+GiovanniEliasDaRosa@users.noreply.github.com>
2026-07-18 10:57:59 -0300

    fix(header): correct image path
    Change image source from `public/assets/icons/logo-light.svg` to `/assets/icons/logo-light.svg"`

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import styles from "./Header.module.css";
 function Header() {
   return (
     <header className={styles.header}>
-      <img src="public/assets/icons/logo-light.svg" alt="" />
+      <img src="/assets/icons/logo-light.svg" alt="" />
       <p>Kanban Board</p>
     </header>
   );
